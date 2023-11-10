e3f0f16139df1d16e68b29638a10cabff5848216
Aadarsh R <aadarsh.r@experionglobal.local>
2023-11-10 10:34:43 +0530

    Fix: Added comments

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 'use strict';
+//Fetching all necessary tags
 const playerX = document.querySelector('.player--0');
 const playerY = document.querySelector('.player--1');
 const playerXName = document.querySelector('#name--0');
 const playerYName = document.querySelector('#name--1');
-playerXName.textContent = String(prompt('Enter Player 1 Name'));
-playerYName.textContent = String(prompt('Enter Player 2 Name'));
 
 const scoreX = document.querySelector('#score--0');
 const scoreY = document.querySelector('#score--1');
@@ -16,11 +15,14 @@ const buttonNew = document.querySelector('.btn--new');
 const buttonRoll = document.querySelector('.btn--roll');
 const buttonHold = document.querySelector('.btn--hold');
 
-let scores, currentScore, activePlayer, playing;
+let scores, currentScore, isActivePlayer, playing; //global variables to use to update scores and check which player is active
 
+//Initialization function to start new game and accept names of each player when game is started
 const init = function () {
+  playerXName.textContent = String(prompt('Enter Player 1 Name'));
+  playerYName.textContent = String(prompt('Enter Player 2 Name'));
   scores = [0, 0];
-  currentScore = activePlayer = 0;
+  currentScore = isActivePlayer = 0;
   playing = true;
 
   scoreX.textContent = 0;
@@ -28,16 +30,18 @@ const init = function () {
   currentX.textContent = 0;
   currentY.textContent = 0;
 
-  diceRoll.classList.add('hidden');
+  diceRoll.classList.add('hidden'); //hiding dice icon when game is started
   playerX.classList.remove('player--winner');
   playerY.classList.remove('player--winner');
   playerX.classList.add('player--active');
   playerY.classList.remove('player--active');
-  document.getElementById('winnerMessage').style.display = 'none';
+  document.getElementById('winnerMessage').style.display = 'none'; //hiding winner message
   const audio = document.getElementById('myAudio');
   audio.pause();
 };
 init();
+buttonNew.addEventListener('click', init);
+//function to generate random dice number
 function Dice() {
   const randomNumber = Math.floor(Math.random() * 6) + 1;
   return randomNumber;
@@ -45,14 +49,15 @@ function Dice() {
 const generateImage = n => {
   document.querySelector('.dice').src = `dice-${n}.png`;
 };
+//function to generate image
 const switchPlayer = function () {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  document.getElementById(`current--${isActivePlayer}`).textContent = 0;
   currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
+  isActivePlayer = isActivePlayer === 0 ? 1 : 0;
   playerX.classList.toggle('player--active');
   playerY.classList.toggle('player--active');
 };
-
+//rolling dice button
 buttonRoll.addEventListener('click', function () {
   if (playing) {
     const dice = Dice();
@@ -62,33 +67,33 @@ buttonRoll.addEventListener('click', function () {
 
     if (dice !== 1) {
       currentScore += dice;
-      document.getElementById(`current--${activePlayer}`).textContent =
+      document.getElementById(`current--${isActivePlayer}`).textContent =
         currentScore;
     } else {
       switchPlayer();
     }
   }
 });
-
+//switching player when hold button is pressed
 buttonHold.addEventListener('click', function () {
   if (playing) {
-    scores[activePlayer] += currentScore;
+    scores[isActivePlayer] += currentScore;
 
-    document.getElementById(`score--${activePlayer}`).textContent =
-      scores[activePlayer];
+    document.getElementById(`score--${isActivePlayer}`).textContent =
+      scores[isActivePlayer];
 
-    if (scores[activePlayer] >= 20) {
+    if (scores[isActivePlayer] >= 20) {
       playing = false;
       diceRoll.classList.add('hidden');
 
       document
-        .querySelector(`.player--${activePlayer}`)
+        .querySelector(`.player--${isActivePlayer}`)
         .classList.add('player--winner');
       document
-        .querySelector(`.player--${activePlayer}`)
+        .querySelector(`.player--${isActivePlayer}`)
         .classList.remove('player--active');
       let playerName;
-      if (activePlayer === 0) {
+      if (isActivePlayer === 0) {
         playerName = playerXName.textContent;
       } else {
         playerName = playerYName.textContent;
@@ -111,7 +116,6 @@ buttonHold.addEventListener('click', function () {
           fireworkStyles[Math.floor(Math.random() * fireworkStyles.length)];
         firework.classList.add(randomStyle);
 
-        // Randomly position the firework within the container
         firework.style.left = Math.random() * 100 + 'vw';
         firework.style.top = Math.random() * 100 + 'vh';
 
@@ -128,5 +132,3 @@ buttonHold.addEventListener('click', function () {
     }
   }
 });
-
-buttonNew.addEventListener('click', init);
